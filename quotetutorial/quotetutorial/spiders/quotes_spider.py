c3787693d27d10cf1db6f10b8898514965350b01
Felipe <felipeiasi@gmail.com>
2019-08-19 18:35:08 -0300

    Quote get!

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -3,10 +3,17 @@ import scrapy
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
-    start_url = [
+    start_urls = [
         'http://quotes.toscrape.com/'
     ]
 
     def parse(self, response):
-        title = response.css('title').extract()
-        yield {'titletext': title}
+        all_div_quotes = response.css('div.quote')
+        title = all_div_quotes.css('span.text::text').extract()
+        author = all_div_quotes.css('span.author::text').extract()
+        tag = all_div_quotes.css('.tag::text').extract()
+        yield {
+            'titletext': title,
+            'author': author,
+            'tag': tag
+        }
