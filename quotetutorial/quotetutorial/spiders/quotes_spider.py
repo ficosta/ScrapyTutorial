70c46621434c9dd0a5519043768e0255dab88edf
Felipe <felipeiasi@gmail.com>
2019-08-20 17:51:36 -0300

    Usando Objeto para armazenar

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
@@ -8,13 +8,15 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        items = QuotetutorialItem()
         all_div_quotes = response.css('div.quote')
         for quote in all_div_quotes:
             title = quote.css('span.text::text').extract()
-            author = quote.css('span.author::text').extract()
+            author = quote.css('.author::text').extract()
             tag = quote.css('.tag::text').extract()
-            yield {
-                'titletext': title,
-                'author': author,
-                'tag': tag
-            }
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
